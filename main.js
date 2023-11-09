72e298fd0ed5fdf5277f227c1301ca591aa18c46
kauan cavalini <110106096+kauanmpx@users.noreply.github.com>
2023-11-09 10:56:38 -0300

    a

--- main.js
+++ main.js
@@ -3,3 +3,51 @@ function tocaSomPom ()  {
 }
 
 document.querrySelector('.tecla_pom').onclick = tocaSomPom();
+
+function tocaSomClap ()  {
+    document.querrySelector('#som_tecla_Clap').play();
+}
+
+document.querrySelector('.tecla_Clap').onclick = tocaSomClap();
+
+function tocaSomTim ()  {
+    document.querrySelector('#som_tecla_tim').play();
+}
+
+document.querrySelector('.tecla_tim').onclick = tocaSomTim();
+
+function tocaSomPuff ()  {
+    document.querrySelector('#som_tecla_puff').play();
+}
+
+document.querrySelector('.tecla_puff').onclick = tocaSomPuff();
+
+function tocaSomSplash ()  {
+    document.querrySelector('#som_tecla_splash').play();
+}
+
+document.querrySelector('.tecla_splash').onclick = tocaSomSplash();
+
+function tocaSomToim ()  {
+    document.querrySelector('#som_tecla_toim').play();
+}
+
+document.querrySelector('.tecla_toim').onclick = tocaSomToim();
+
+function tocaSomPsh ()  {
+    document.querrySelector('#som_tecla_psh').play();
+}
+
+document.querrySelector('.tecla_psh').onclick = tocaSomPsh();
+
+function tocaSomTic ()  {
+    document.querrySelector('#som_tecla_tic').play();
+}
+
+document.querrySelector('.tecla_tic').onclick = tocaSomTic();
+
+function tocaSomTom ()  {
+    document.querrySelector('#som_tecla_tom').play();
+}
+
+document.querrySelector('.tecla_tom').onclick = tocaSomTom();
